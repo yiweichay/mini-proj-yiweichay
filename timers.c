c2a3ee117f09d7562ed5d4f47a6e84ae9aa7149d
yiweichay <92339387+yiweichay@users.noreply.github.com>
2021-11-09 15:48:15 +0000

    Update timers.c

--- timers.c
+++ timers.c
@@ -8,7 +8,7 @@ void Timer0_init(void)
 {
     T0CON1bits.T0CS=0b010; // Fosc/4
     T0CON1bits.T0ASYNC=1; // see datasheet errata - needed to ensure correct operation when Fosc/4 used as clock source
-    T0CON1bits.T0CKPS=0b0000; // 1:256 (prescaler))
+    T0CON1bits.T0CKPS=0b1000; // 1:256 (prescaler))
     T0CON0bits.T016BIT=1;	//8bit mode	
 	
     // it's a good idea to initialise the timer registers so we know we are at 0
